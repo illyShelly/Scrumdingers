265f11c9d6b56c6ef52d182d998867375e99e6a5
Ilona Sellenberk <ilona.sell@gmail.com>
2023-03-17 20:43:35 +0100

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Ilona Sellenberkova on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
